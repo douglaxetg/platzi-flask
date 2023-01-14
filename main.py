f79ed3b4f5eb946b23c4f6337854ee20aeff2220
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-14 00:50:37 -0400

    add test_base, there are some issues with assertRedirects

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -14,6 +15,11 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
